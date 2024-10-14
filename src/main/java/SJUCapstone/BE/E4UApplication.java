89f7d65ffc8dee14cc0048c52f27c19c15aba674
Han <hkjbrian@naver.com>
2024-10-14 14:23:47 +0900

    feat : 프로젝트 초기 설정

---
+++ src/main/java/SJUCapstone/BE/E4UApplication.java
@@ -0,0 +1,11 @@
+package SJUCapstone.BE;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E4UApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E4UApplication.class, args);
+    }
+}
