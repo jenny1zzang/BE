ede81d27b04bf99f79b3375194791b3f8cb08898
jenny1zzang <cyberkhl@naver.com>
2024-10-17 14:24:35 +0900

    feat: Diagnosis 관련 기능 추가

---
+++ src/test/java/com/example/E4U/E4UApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E4U;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E4UApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
