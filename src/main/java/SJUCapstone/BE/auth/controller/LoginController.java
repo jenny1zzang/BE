94b7d4644f0a79a5c98a977338b6e5d41662af4a
Han <hkjbrian@naver.com>
2024-11-11 17:31:59 +0900

    refactor : 스웨거 설정 위해@RestController로 변경

--- src/main/java/SJUCapstone/BE/auth/controller/LoginController.java
+++ src/main/java/SJUCapstone/BE/auth/controller/LoginController.java
@@ -11,11 +11,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class LoginController {
+
     @Autowired
     LoginService loginService;
 
--- src/main/java/SJUCapstone/BE/user/controller/UserController.java
+++ src/main/java/SJUCapstone/BE/user/controller/UserController.java
@@ -9,12 +9,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
     @Autowired
     UserService userService;
