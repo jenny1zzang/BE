3c6e4a2211f41101998fe38a12cbf3f4cecd0b19
Han <hkjbrian@naver.com>
2024-11-24 11:35:17 +0900

    chore : Swagger 설정 on

--- src/main/java/SJUCapstone/BE/SwaggerConfig.java
+++ src/main/java/SJUCapstone/BE/SwaggerConfig.java
@@ -1,21 +1,21 @@
-//package SJUCapstone.BE;
-//
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.servers.Server;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        Server server = new Server();
-//        server.setUrl("https://e4u.kro.kr"); // HTTPS 프로토콜 사용
-//        server.setDescription("Production Server");
-//
-//        return new OpenAPI()
-//                .addServersItem(server);
-//    }
-//
-//}
+package SJUCapstone.BE;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://e4u.kro.kr"); // HTTPS 프로토콜 사용
+        server.setDescription("Production Server");
+
+        return new OpenAPI()
+                .addServersItem(server);
+    }
+
+}
